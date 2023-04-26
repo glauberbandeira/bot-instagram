ee167d096c208d0c96fa8bcceed0de6cf5bffdbd
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-27 00:01:40 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import webbrowser
+import pyautogui
+from time import sleep
+
+webbrowser.open_new_tab('https://www.instagram.com')
+sleep(1)
+
+pyautogui.moveTo(823,545,duration=1)
+sleep(1)
+pyautogui.click(823,545,duration=1)
+sleep(1)
+
+
+pyautogui.click(759,304,duration=1)
+pyautogui.typewrite('glauberbandeiradev')
+sleep(1)
+
+pyautogui.click(773,346,duration=1)
+pyautogui.typewrite('Gb@8910*')
+sleep(1)
+
+pyautogui.click(821,399, duration=1)
+sleep(2)
+
+pyautogui.click(795,464, duration=1)
+sleep(1)
+
+# Pesquisar Página / Search page
+pyautogui.click(75,288,duration=1)
+sleep(1)
+
+pyautogui.click(101,216,duration=1)
+sleep(1)
+pyautogui.typewrite('premier group recruitment')
+sleep(1)
+
+
+
+
